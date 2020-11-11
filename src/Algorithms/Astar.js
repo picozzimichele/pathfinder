92a68ba96a7a87b7ad0c3a56f0896a3f0694ada2
picozzimichele <michele.picozzi@hotmail.it>
2020-11-11 20:46:59 +0000

    created Astar algo functioning

--- src/Algorithms/Astar.js
+++ src/Algorithms/Astar.js
@@ -1,7 +1,77 @@
+function heruistic(a, b) 
+{
+    let d = Math.abs(a.x - a.y) + Math.abs(b.x - b.y) //calculating the distance using the Manhattan method also called taxi driver
+    return d
+}
+
+
 function Astar(startNode, endNode) 
 {
-    var openSet = [];
-    var closedSet = [];
+    var openSet = []; //this contains all the Nodes/Spot we still have to visit
+    var closedSet = []; //this contains the Nodes that we have already visited
+    let path = []; //this will contain the shortest path to our end node
+
+    openSet.push(startNode) //our starting point we push this immediately
+
+    while(openSet.length > 0) //this loops while our openSet still has nodes to check
+    {
+        let leastIndex = 0; //this is the index with the lowest f score
+        for(let i = 0; i < openSet.length; i++)
+        {
+            if(openSet[i].f < openSet[leastIndex].f) //this compares the two f(distances) scores of the nodes
+            {
+                leastIndex = i; //i f score is better so new leastIndex is replaced by this new node getting us the node with the lowest f value
+            }
+        }
+
+        let current = openSet[leastIndex]; //this is the current lowest f node that we found
+
+        if(current === endNode) // we have found the endNode and path
+        {
+            console.log("Path Found!")
+        }
+
+        openSet = openSet.filter(elt => elt !== current) //this removes the current from the openSet (we have one less node to check) there is no remove function unfortunately
+        closedSet.push(current) //adding then the current to the closedSet which was removed from openSet
+
+        console.log(current) //DEBUGGING
+
+        let neighbours = current.neighbours; //we need to check all the 4 neighbours nodes of the current node
+        console.log(neighbours) //DEBUGGING
+        for(let i = 0; i < neighbours.length; i++)
+        {
+            let neighbour = neighbours[i]; //exctracting each of the 4 neighbour from the different array index
+            console.log(neighbour) //RETURNS UNDEFINED
+            if(!closedSet.includes(neighbour)) //if closedSet does not include the neighbour
+            {
+
+                let tempG = current.g + 1 //temporary G value
+                let newPath = false;
+
+                if(openSet.includes(neighbour))//if openSet includes that neighbour
+                {
+                    if(tempG < neighbour.g) //if the tempG (current.g +1) is lower than the neighbour g value
+                    {
+                        neighbour.g = tempG; //set the neighbour.g to current.g +1
+                        newPath = true;
+                    }
+                }else 
+                {
+                    neighbour.g = tempG; //set the neighbour.g to current.g +1
+                    newPath = true;
+                    openSet.push(neighbour); //add this neighbour to the openSet again since in this case is not part of it
+                }
+
+                if(newPath) //if newPath is true, meaning we have found a newPath
+                {
+                    neighbour.h = heruistic(neighbour, endNode) //basically calculating the assumed distance from where we are to the end
+                    neighbour.f = neighbour.h + neighbour.g //recalculating the toal distance the f value
+                    neighbour.previous = current; //previous is a value in the Spot that starts as undefined check pathfinder.js
+                }
+            }
+        }
+    }
 }
+
 
 export default Astar;
--- src/components/Pathfinder.js
+++ src/components/Pathfinder.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react"
 import "../styles/Pathfinder.css"
 import Node from "./Node"
-import Astar from "../Algorithms/Astar"
+import Astar from "../Algorithms/astar"
 
 
 let cols = 50;
-let rows = 25;
-var grid = new Array(cols);
+let rows = 20;
+var grid = new Array(rows);
 
 var startRow = 0;
 var startCol = 0;
@@ -14,26 +14,13 @@ var endRow = rows - 1;
 var endCol = cols - 1;
 
 
-//constructing the Spot
-function Spot(i, j) {
-    this.x = i //row
-    this.y = j //column
-    this.f = 0;
-    this.g = 0;
-    this.h = 0;
-
-    this.isStart = this.x === startRow && this.y === startCol
-    this.isEnd = this.x === endRow && this.y === endCol
-
-}
-
 const Pathfinder = () => {
 
     const [Grid, setGrid] = useState([]);
 
     // this gets called at the beginning to construct the grid
     useEffect(() => {
-
+        
         initializeGrid(grid)
 
     }, [])
@@ -41,12 +28,25 @@ const Pathfinder = () => {
     const initializeGrid = () => 
     {
         //Making a 2D array of cols with rows things in it
-        for(let i = 0; i < cols; i++)
+        for(let i = 0; i < rows; i++)
         {
-            grid[i] = new Array(rows)
+            grid[i] = new Array(cols)
         }
+        
+        createSpot(grid)
 
-        //another loop for every colum and rows and creates the Spot(i, j)
+        setGrid(grid) //refer to useState above basically Grid = grid
+
+        addNeighbours(grid)
+
+        const startNode = grid[startRow][startCol]
+        const endNode = grid[endRow][endCol]
+
+        Astar(startNode, endNode)
+    }
+
+    //another loop for every colum and rows and creates the Spot(i, j)
+    const createSpot = (grid) => {
         for(let i = 0; i < rows; i++)
         {
             for(let j = 0; j < cols; j++)
@@ -54,8 +54,44 @@ const Pathfinder = () => {
                 grid[i][j] = new Spot(i, j);
             }
         }
+    }
 
-        setGrid(grid) //refer to useState above basically Grid = grid
+    //constructing the Spot
+    function Spot(i, j) {
+        this.x = i //row
+        this.y = j //column
+        this.f = 0;
+        this.g = 0;
+        this.h = 0;
+
+        this.isStart = this.x === startRow && this.y === startCol
+        this.isEnd = this.x === endRow && this.y === endCol
+
+        //neighbours to each spot (should have up to 4)
+        this.neighbours = [];
+        this.previous = undefined;
+
+        this.addneighbours = function(grid)
+        {   // i is the row spot
+            let i = this.x //row
+            let j = this.y //column
+            //this adds all the neighbour spots into this.neighbour array above
+            if(i > 0) this.neighbours.push(grid[i - 1][j]);
+            if(i < (rows - 1)) this.neighbours.push(grid[i + 1][j]);
+            if(j > 0) this.neighbours.push(grid[i][j - 1])
+            if(j < (cols - 1)) this.neighbours.push(grid[i][j + 1]);
+        }
+    }
+
+    // this add the neighbour spots to each grid spot
+    const addNeighbours = (grid) => {
+        for(let i = 0; i < rows; i++)
+        {
+            for(let j = 0; j < cols; j++)
+            {
+                grid[i][j].addneighbours(grid)
+            }
+        }
     }
     
 
